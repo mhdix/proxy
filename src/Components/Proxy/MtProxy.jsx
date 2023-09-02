04c038ecdf7f3a737c6fa2c0cfc0c31c96e7a121
mahdi <domanloom@gmail.com>
2023-09-02 19:12:56 +0330

    change many style

--- src/Components/Proxy/MtProxy.jsx
+++ src/Components/Proxy/MtProxy.jsx
@@ -4,24 +4,41 @@ import './MtProxy.css'
 const MtProxy = () => {
     const [proxy, setProxy] = useState([])
     const [onClicked, setOnClicked] = useState()
+    const [ping,setPing] = useState(null)
     console.log(proxy);
     useEffect(() => {
         async function proxy() {
             const pro = await axios.get('https://raw.githubusercontent.com/hookzof/socks5_list/master/tg/mtproto.json')
             setProxy(pro.data)
+            // https://web.telegram.org/k/
+            setPing(proxy.ping)
         }
         proxy()
-    }, [])
+        console.log(ping);
+    }, [ping])
     return (
         <div className='proxy'>
             {
                 proxy.map((item) => {
                     return (
                         <div className='proxy-box'>
-                            <p>host : {item.host}</p>
-                            <p>port : {item.port}</p>
-                            <p>ping : {item.ping}</p>
-                            <p>secret : {item.secret}</p>
+                            <div>
+                                <p>host : {item.host}</p>
+                                <button onClick={() => navigator.clipboard.writeText(`${item.host}`)}>copy</button>
+                            </div>
+                            <div>
+                                <p>port : {item.port}</p>
+                                <button onClick={() => navigator.clipboard.writeText(`${item.port}`)}>copy</button>
+
+                            </div>
+                            <div>
+                                <p>secret : {item.secret}</p>
+                                <button onClick={() => navigator.clipboard.writeText(`${item.secret}`)}>copy</button>
+
+                            </div>
+                            <div>
+                                <p className={item.ping > 1000 ? 'red' : 'green'} >ping : {item.ping}</p>
+                            </div>
                         </div>
                     )
                 })
