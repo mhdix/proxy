671ebeaf7d8c8faec9751e45a25fde35c6ede777
mahdi <domanloom@gmail.com>
2023-09-02 12:25:35 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import MtProxy from './Components/Proxy/MtProxy';
+
+function App() {
+
+  return (
+    <div className='main'>
+      <MtProxy />
+    </div>
+  )
+}
+
+export default App
